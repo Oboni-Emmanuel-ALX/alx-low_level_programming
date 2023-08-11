d6dfefb3a1119c23c266ce6e430cf894ac76fa21
Oboni-Emmanuel-ALX <emmanueloboni02@outlook.com>
2023-08-11 11:20:20 -0700

    Fixd: line over 80 characters (82)

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,23 +10,23 @@
 int main(void)
 {
 	int n;
-	int last_digit;
+	int last_num;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* Print the last digit by taking the remainder when divided by 10 */
-	last_digit = n % 10;
-	if (last_digit > 5)
+	last_num = n % 10;
+	if (last_num > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_num);
 	}
-	else if (last_digit == 0)
+	else if (last_num == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, last_num);
 	}
-	else if (last_digit < 6 && last_digit != 0)
+	else if (last_num < 6 && last_num != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_num);
 	}
 		return (0);
 }
