6e6bd6910f297acfebf48113abca00879ab7ed09
Oboni-Emmanuel-ALX <emmanueloboni02@outlook.com>
2023-08-13 02:29:14 -0700

    Single number seperated by ',',' ' & '$' attached to 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s_num;
+
+	for (s_num = 0; s_num < 10; s_num++)
+	{
+		putchar(s_num + '0');
+		if (s_num <= 9)
+		{
+			putchar((s_num == 9) ? '$' : ',');
+			putchar((s_num == 9) ? '\n' : ' ');
+		}
+	}
+	return (0);
+}
