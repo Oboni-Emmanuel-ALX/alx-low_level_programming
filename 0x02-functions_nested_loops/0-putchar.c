1fb7a0efb5575eb00a38c057ce91c099920d1f41
Oboni-Emmanuel-ALX <emmanueloboni02@outlook.com>
2023-08-15 10:10:49 -0700

    program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/* Function declaration */
+int _putchar(void);
+/**
+ * main - Entry point
+ * Description: program
+ *
+ * Return: O (Success)
+ */
+int main(void)
+{
+	write(1, "_putchar\n", 9);
+	return (0);
+}
