1a35a32a91f37ce8d137ad1dd51fd479ecd9210c
Oboni-Emmanuel-ALX <emmanueloboni02@outlook.com>
2023-08-17 11:30:00 -0700

    Function that multiplies two integers.

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,8 @@
 
 /**
  * mul - Function's entry point
- * @a, @b: parameters to be multiplied
+ * @a: First parameter
+ * @b: second parameter
  * Return: Always 0 (Success);
  */
 int mul(int a, int b)
