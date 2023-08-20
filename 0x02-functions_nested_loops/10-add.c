322bc61819c8723569576fd9fc99e3c9cf7b90b8
Oboni-Emmanuel-ALX <emmanueloboni02@outlook.com>
2023-08-20 04:37:44 -0700

    10. a+b - Function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: first integer
+ * @b: Second integer
+ * Return: result (Success)
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
