293fd6c35b76155b17567c769d14dbb5347173e2
Oboni-Emmanuel-ALX <emmanueloboni02@outlook.com>
2023-08-21 13:46:06 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,15 @@
  */
 void puts2(char *str)
 {
-	int  i = 0;
+	int  len = 0;
 
-	while (str[i] != '\0')
+	while (str[len] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (len % 2 == 0)
+		{
+			_putchar(str[len]);
+		}
+		len++;
 	}
 	_putchar('\n');
 }
