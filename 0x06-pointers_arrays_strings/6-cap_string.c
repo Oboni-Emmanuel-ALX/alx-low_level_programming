0d858047c2fd0b582114dc24b14e1117ea8fc7db
Oboni-Emmanuel-ALX <emmanueloboni02@outlook.com>
2023-08-24 14:00:03 -0700

    function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string.
+ * @strr: pointer
+ * Return: strr.
+ */
+char *cap_string(char *strr)
+{
+	int indexx = 0;
+
+	while (strr[indexx])
+	{
+		while (!(strr[indexx] >= 'a' && strr[indexx] <= 'z'))
+			indexx++;
+		if (strr[indexx - 1] == ' ' ||
+		    strr[indexx - 1] == '\t' ||
+		    strr[indexx - 1] == '\n' ||
+		    strr[indexx - 1] == ',' ||
+		    strr[indexx - 1] == ';' ||
+		    strr[indexx - 1] == '.' ||
+		    strr[indexx - 1] == '!' ||
+		    strr[indexx - 1] == '?' ||
+		    strr[indexx - 1] == '"' ||
+		    strr[indexx - 1] == '(' ||
+		    strr[indexx - 1] == ')' ||
+		    strr[indexx - 1] == '{' ||
+		    strr[indexx - 1] == '}' ||
+		    indexx == 0)
+			strr[indexx] -= 32;
+		indexx++;
+	}
+
+	return (strr);
+}
+
